97fa17c32d7b9527993580a8f73f372452129d74
Pavlo Lazarenko <189833560+loweffortmisery@users.noreply.github.com>
2025-10-19 13:32:46 +0000

    Add function to read polygons from text files and support for text files in output function

--- file_forming.c
+++ file_forming.c
@@ -66,20 +66,58 @@ Polygone* readPolygones(FILE* fp) {
     }
     return polygones;
 }
+    
+Polygone* readPolygones_from_text(FILE* fp) {
+    assert(fp);
+    unsigned int n;
+    fscanf(fp, "%u", &n);
+    Polygone* polygones = (Polygone*) malloc(n * sizeof(Polygone));
+    Polygone p;
+    for (int i = 0; i < n; i++) {
+        fscanf(fp, "%u", &p.n);
+        p.vertice = (TPoint*) malloc(p.n * sizeof(TPoint));
+        for (int j = 0; j < p.n; j++) {
+            fscanf(fp, "%f %f", &p.vertice[j].x, &p.vertice[j].y);
+        }
+        polygones[i] = p;
+    }
+    return polygones;
+}
 
 void output() {
     // to print put all polygones from the file
     char filename[LEN];
     printf("Enter a name of the file to read polygons from : ");
     scanf("%s", filename);
-    FILE* fp = fopen(filename, "r");
-    Polygone* polygones = readPolygones(fp);
-    int i = 0;
-    while(1) {
-        if (polygones[i].n == 0) {
-            break;
+   
+    const char *ext = strrchr(filename, '.');
+    int is_binary = ext && strcmp(ext, ".bin") == 0;
+    int is_text = ext && strcmp(ext, ".txt") == 0;
+    if (!is_binary && !is_text) {
+        printf("Error: File %s must have .bin or .txt extension.\n", filename);
+        return;
+    }
+
+    FILE *fp = fopen(filename, is_binary ? "rb" : "r");
+    if (!fp) {
+        printf("Error: Cannot open file %s\n", filename);
+        return;
+    }
+
+    unsigned int count = 0;
+    if (is_binary) {
+        fread(&count, sizeof(NTYPE), 1, fp_a);
+    } else {
+        fscanf(fp_a, "%u", &count);
+    }
+
+    Polygone *polygones = (is_binary ? readPolygones(fp) : readPolygones_from_text(fp));
+    
+    if (polygones) {
+        for (unsigned int i = 0; i < count; i++) {
+            outputPolygon(polygones[i]);
+            free_polygone(&polygones[i]);
         }
-        outputPolygon(polygones[i]);
-        i++;
+        free(polygones);
     }
 }
